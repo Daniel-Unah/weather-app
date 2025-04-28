8e3fb3ddc1654b0bcc95e78235c7d913919153a7
Daniel <106980147+Daniel-Unah@users.noreply.github.com>
2025-04-28 15:21:52 -0500

    added in async fetch weather data function

--- src/weatherWidget.jsx
+++ src/weatherWidget.jsx
@@ -1,2 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import API_KEY from './config.js'
+
+const WeatherWidget = () => {
+    const [city, setCity] = useState("")
+    const [weatherData, setWeatherData] = useState(null)
+
+    const PATHWAY = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+    const fetchWeatherData = async () => {
+        try {
+            const response = await fetch(PATHWAY)
+            setWeatherData(response.data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+}
